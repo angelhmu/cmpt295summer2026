dbacc870da2aec7fe12429e9ac920c013cf52b1d
angelhmu <angelhmu@cs.sfu.ca>
2026-07-17 13:19:44 -0700

    stuff

--- lab6/utf8.c
+++ lab6/utf8.c
@@ -17,18 +17,18 @@ void decode_utf8(const char* s) {
             val |= ((uint8_t)s[i+1] & 0x3F);    // remove first 2 bits (prefix)
             report_character(val, 2);
             i += 2;
-        } else if ((s[i] & 0xF0) == 0xE0) {             // 1110xxxx 10xxxxxx 10xxxxxx
+        } else if ((s[i] & 0xF0) == 0xE0) {     // 1110xxxx 10xxxxxx 10xxxxxx
             // the three-byte case
             val = ((uint8_t)s[i] & 0x0F) << 12; // remove first 4 bits (prefix), shift left by 12
             val |= ((uint8_t)s[i+1] & 0x3F) << 6;// remove first 2 bits (prefix), shift left by 6
             val |= ((uint8_t)s[i+2] & 0x3F);    // remove first 2 bits (prefix)
             report_character(val, 3);
             i += 3;
-        } else if ((s[i] & 0xF8) == 0xF0) {               // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+        } else if ((s[i] & 0xF8) == 0xF0) {     // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
             // the four-byte case
             val = ((uint8_t)s[i] & 0x07) << 18; // remove first 5 bits (prefix), shift left by 18
             val |= ((uint8_t)s[i+1] & 0x3F) << 12;// remove first 2 bits (prefix), shift left by 12
-            val |= ((uint8_t)s[i+2] & 0x3F) << 6;    // remove first 2 bits (prefix), shift left by 6
+            val |= ((uint8_t)s[i+2] & 0x3F) << 6;// remove first 2 bits (prefix), shift left by 6
             val |= ((uint8_t)s[i+3] & 0x3F);    // remove firt 2 bytes (prefix)
             report_character(val, 4);
             i += 4;
