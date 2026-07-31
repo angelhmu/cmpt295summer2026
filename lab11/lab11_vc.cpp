fc5f14919e396ccebdfa21e3273d8523165e836f
angelhmu <angelhmu@cs.sfu.ca>
2026-07-31 14:30:19 -0700

    partial lab11 implementations

---
+++ lab11/lab11_vc.cpp
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include "vectorclass/vectorclass.h"
+#include "lab11.h"
+
+double dot_double_vc(double* arr1, double* arr2, uint64_t n) {
+    double total = 0.0;
+    return total;
+}
+
+float dot_single_vc(float* arr1, float* arr2, uint64_t n) {
+    float total = 0.0;
+    return total;
+}
+
+void map_poly_double_vc(double* input, double* output, uint64_t length, double a, double b, double c, double d) {
+    
+}
+void map_poly_single_vc(float* input, float* output, uint64_t length, float a, float b, float c, float d) {
+
+}
